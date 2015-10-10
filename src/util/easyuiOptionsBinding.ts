58bced4fd5009cddcdc1cac232e649c81ccad2af
wengys <wengyuansheng@hotmail.com>
2015-10-10 10:26:04 +0800

    修正ts定义
    修正更新typescript版本后暴露出的定义问题。无功能改动
    treegrid实在提不起兴致搞啊……所有和easyui tree相关的玩意都很蛋疼

--- src/util/easyuiOptionsBinding.ts
+++ src/util/easyuiOptionsBinding.ts
@@ -1,6 +1,6 @@
 ﻿/// <reference path="../typings/knockout/knockout.d.ts" />
 
-ko.bindingHandlers["easyuiOptions"] = {
+ko.bindingHandlers["easyuiOptions"] = <KnockoutBindingHandler>{
     init: (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) => { },
     update: (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) => { },
     easyuiOptionsVersion: '0.6.0'
